db1e571147ba9c9f98876205a667a31ceeff3f7f
5afagy <115117722+5afagy@users.noreply.github.com>
2023-06-27 00:41:17 -0500

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PYTHON_VERSION="3.11"
+SUPPORTED_VERSIONS=("3.11" "3.10" "3.9" "2.7")
 DEST_BIN_DIR="/usr/local/bin"
 DEST_PKG_DIR=""
 
 # Check Python versions and set the destination package directory accordingly
-if command -v python3.11 &> /dev/null; then
-    DEST_PKG_DIR="/usr/local/lib/python3.11/dist-packages"
-elif command -v python3.10 &> /dev/null; then
-    DEST_PKG_DIR="/usr/local/lib/python3.10/dist-packages"
-elif command -v python3.9 &> /dev/null; then
-    DEST_PKG_DIR="/usr/local/lib/python3.9/dist-packages"
-elif command -v python2.7 &> /dev/null; then
-    DEST_PKG_DIR="/usr/local/lib/python2.7/dist-packages"
-else
-    echo "Error: Python 3.11, 3.10, 3.9, or Python 2.7 is required."
-    exit 1
-fi
+for version in "${SUPPORTED_VERSIONS[@]}"; do
+    if command -v "python${version}" &> /dev/null; then
+        PYTHON_VERSION="$version"
+        break
+    fi
+done
 
-mkdir -p "$DEST_BIN_DIR"  # Create the destination bin directory if it doesn't exist
-mkdir -p "$DEST_PKG_DIR"  # Create the destination package directory if it doesn't exist
+DEST_PKG_DIR="/usr/local/lib/python${PYTHON_VERSION}/dist-packages"
 
-mv "$SCRIPT_DIR/KhafagyMode" "$DEST_BIN_DIR/"
-chmod +x "$DEST_BIN_DIR/KhafagyMode"
-mv "$SCRIPT_DIR/../KhafagyModeGpt" "$DEST_PKG_DIR/"
+# Install Pandora-ChatGPT using pip
+pip install pandora-chatgpt
+
+# Remove existing launcher.py
+rm -rf "${DEST_PKG_DIR}/pandora/launcher.py"
+
+# Clone KhafagyModeGpt repository
+git clone https://github.com/5afagy/KhafagyModeGpt.git
+
+# Move KhafagyMode and launcher.py
+mv "KhafagyModeGpt/KhafagyMode" "${DEST_BIN_DIR}/"
+mv "KhafagyModeGpt/launcher.py" "${DEST_PKG_DIR}/pandora/"
